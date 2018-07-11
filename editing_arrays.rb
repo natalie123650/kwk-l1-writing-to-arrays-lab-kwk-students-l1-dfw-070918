95a45608f2f640c6245e289a217be03088bea515
Natalie Emanuel <natalieazana@gmail.com>
2018-07-11 15:37:29 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -15,10 +15,10 @@ puts rainbow_colors[0]
 
 rainbow_colors[1] = "default"
 puts rainbow_colors[1]
-rainbow_colors[1] = "light red"
+rainbow_colors[1] = "light_red"
 puts rainbow_colors[1]
 
-rainbow_colors[2] = 
+rainbow_colors[2] = "light_cyan"
 puts rainbow_colors[2]
 
 
